2e6e70cc7aa904d56b4132baffe786c84fc75781
Aditya948351 <devpathind.community@gmail.com>
2026-05-10 21:55:03 +0530

    security: untrack .firebase and remove Hackfiesta event files

--- hackfiestacertificates/server/src/routes/certificateRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const upload = require('../config/multer');
-const { generateCertificates, generateSingle } = require('../controllers/certificateController');
-
-router.post('/generate', upload.single('file'), generateCertificates);
-router.post('/generate-single', generateSingle);
-
-module.exports = router;
